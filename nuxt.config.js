d13a6a47b99d24a402715b41321471fc412e40a9
Seongjoo Jin <evanjin8@gmail.com>
2019-02-09 13:35:55 +0900

    first commit

---
+++ nuxt.config.js
@@ -0,0 +1,67 @@
+const pkg = require("./package")
+
+module.exports = {
+  mode: "universal",
+
+  /*
+   ** Headers of the page
+   */
+  head: {
+    title: pkg.name,
+    meta: [
+      { charset: "utf-8" },
+      { name: "viewport", content: "width=device-width, initial-scale=1" },
+      { hid: "description", name: "description", content: pkg.description }
+    ],
+    link: [{ rel: "icon", type: "image/x-icon", href: "/favicon.ico" }]
+  },
+
+  /*
+   ** Customize the progress-bar color
+   */
+  loading: { color: "#fff" },
+
+  /*
+   ** Global CSS
+   */
+  css: ["ant-design-vue/dist/antd.css"],
+
+  /*
+   ** Plugins to load before mounting the App
+   */
+  plugins: ["@/plugins/antd-ui"],
+
+  /*
+   ** Nuxt.js modules
+   */
+  modules: ["@nuxtjs/pwa"],
+
+  manifest: {
+    name: "nuxt_pwa",
+    short_name: "NuxtPwa",
+    start_url: "/?utm_source=homescreen",
+    display: "standalone",
+    background_color: "#000"
+  },
+  workbox: {
+    dev: true,
+    offline: false,
+    runtimeCaching: [
+      {
+        urlPattern: "/*",
+        handler: "networkFirst",
+        method: "GET"
+      }
+    ]
+  },
+
+  /*
+   ** Build configuration
+   */
+  build: {
+    /*
+     ** You can extend webpack config here
+     */
+    extend(config, ctx) {}
+  }
+}
